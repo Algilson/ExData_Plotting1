3f34cf91ce8d7b882a9167f34b3f7f14e42920f6
Algilson <algilson3@gmail.com>
2016-01-10 16:21:29 -0500

    Create Plot2.R

---
+++ Plot2.R
@@ -0,0 +1,5 @@
+#add to the existing data in Plot1.R
+#use the plot function with pre existing variables and define the x and y axis
+> plot(electric_power_data_revised$SetTime, electric_power_data_revised$Global_active_power, type="l",col="black",xlab="", ylab="Global Active Power (kilowatts)" 
++ )
+> 
